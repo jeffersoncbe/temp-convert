6ffb24a16e29c49c1b8b908670ac44ca2d06f26a
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2025-06-03 20:16:58 -0300

    Criando botões e retornando com o resultado

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function converterCtoF(tempC) {
     const tempF = (tempC * 9/5) + 32
     console.log(`Temperatura = ${tempF} F`);
+
+    return tempF;
 }
 
-converterCtoF(23)
+function converter() {
+    console.log('Ei! Clicou!')
+    let tempC = document.getElementById('tempC').value
+    console.log(`Temp ºC: ${tempC}`);
+
+    // Chamando afunçaõ que converte de Celcius para Farenheit
+
+    const resultado = converterCtoF(tempC);
+
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+
