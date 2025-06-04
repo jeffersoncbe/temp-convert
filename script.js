2cba73497002b69552dc3d939fe11fe6ebee1e0f
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2025-06-03 22:21:43 -0300

    Termometro corrigido

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 function converterCtoF(tempC) {
-    const tempF = (tempC * 9/5) + 32
-    console.log(`Temp. em Fahrenheit = ${tempF} F`);
+    const tempF = (tempC * 9 / 5) + 32;
+    console.log(`Temp. em Farenheit = ${tempF} F`);
+    /* o return retorna o que é desejado, neste caso a temp convertida que está na variável tempF */
     return tempF;
 }
 
 function converterCtoK(tempC) {
-    const tempK = tempC + 273.15
-    console.log(`Temp. em Kelvin = ${tempK} K`);
+    const tempK = tempC + 273.15;
+    console.log(`Temp. em Kelvin = ${tempK} k`);
+
     return tempK;
 }
 
-function converter(temp) {}
-    console.log('Ei! Clicou!')
+function converter(temp) {
+    console.log('Ei Clicou!', temp);
 
- if (temp === 'F') {
+    /* parseFloat() converte para decimal, pois estava vindo no formato de string, exemplo '18' */
     let tempC = parseFloat(document.getElementById('tempC').value);
-    console.log(`Temp ºC: ${tempC}`);
+    let resultado = 0;
 
-    // Mostrar Resultado
+    console.log(`Temp Celsius: ${tempC} °C`);
+
+    if (temp === 'F') {
+        /* Chamando a função que converte de C para F */
+        resultado = converterCtoF(tempC);
+    } else if (temp === 'K') {
+        /* Chamando a função que converte de C para K */
+        resultado = converterCtoK(tempC);
+    }
+
+    /* Exibe o reultado na tag que possui um id chamado resultado no HTML */
+    document.getElementById('resultado').innerHTML = `${resultado} ${temp}`;
+
+    /* Exibir a div com a temperatura assim que o valor estiver setado em F */
     document.getElementsByClassName('resultado')[0].style.display = 'flex'
 
- 
-    // Chamando a função que converte de Celcius para Farenheit
-
-    const resultado = converterCtoF(tempC);
-    document.getElementById('resultado').innerHTML = `${resultado} F`; 
-
-
-    const resultadoK = converterCtoK(tempC);
-    document.getElementById('resultado').innerHTML = `${resultadok} K`; 
-    
 }
 
 function limpar() {
-    console.log('Limpou!')
+    console.log('limpou!');
+    //document.getElementById('resultado').innerHTML = ''
 
-    // Limpar/Ocultar Resultado
+    /* Limpa (Oculta) */
     document.getElementsByClassName('resultado')[0].style.display = 'none'
-
-} 
+}
